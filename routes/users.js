15da47544bf5cc4d2cde0bd7bd7cde463746487c
Sai Sashreek Dittakavi <53420544+saisashreek5thdt@users.noreply.github.com>
2022-10-20 10:16:57 +0530

    Branch Updated with latest Codes

--- routes/users.js
+++ routes/users.js
@@ -2,27 +2,42 @@
 const [router, path] = [require("express").Router(), require("path")];
 
 // Controllers
-const { UserRegistration, UserRolesRegistration, GetAllRegisteredUsers, UserRolesAssign } = require(path.join(
-  __dirname,
-  "..",
-  "controllers",
-  "users"
-));
+const {
+  UserRegistration,
+  GetAllRegisteredUsers,
+  GetOneRegisterUserById,
+  UserLogin,
+  OTPAuthentication,
+  UpdateRegisteredUser,
+  DeleteRegisteredUser
+} = require(path.join(__dirname, "..", "controllers", "users"));
 
 // Middlewares
-const { UserRegisterValidation, UserRolesValidation } = require(path.join(
+const { UserRegisterValidation } = require(path.join(
   __dirname,
   "..",
   "middlewares",
   "validators"
 ));
+const { isUserAuthorized } = require(path.join(
+  __dirname,
+  "..",
+  "middlewares",
+  "isAuthorized"
+));
 
 router.post("/register", UserRegisterValidation, UserRegistration);
 
-router.get('/all', GetAllRegisteredUsers)
+router.get("/all", GetAllRegisteredUsers);
 
-router.post("/userroleregister", UserRolesValidation, UserRolesRegistration);
+router.get("/user/:registerid", GetOneRegisterUserById);
 
-router.post("/userroleassign/:userroleid/:userid", UserRolesAssign);
+router.put("/login", UserLogin);
+
+router.post("/otp", OTPAuthentication);
+
+router.put("/user/update/:registerid", isUserAuthorized, UpdateRegisteredUser);
+
+router.delete("/user/delete/:registerid", isUserAuthorized, DeleteRegisteredUser);
 
 module.exports = router;
